51caa121eafa8ccd1ebade568cc1f26eade35a0e
ruansheng <825307513@qq.com>
2015-03-30 14:27:36 +0800

    write key-value

--- ParseConf.c
+++ ParseConf.c
@@ -14,6 +14,51 @@ typedef struct Line NODE;
 
 NODE *Lines;
 
+/**
+ * 过滤#注释
+ * @return 返回过滤后的字符串
+ */
+char * filterSharp(char *str,int len){
+	char tmp[len];
+	memset(tmp,0,len);
+	int i=0;
+	while(*str!='\0'){
+		if(*str=='#'||*str=='\n'){
+			break;
+		}else if(*str=='\t'){
+			str++;
+			continue;
+		}else{
+			tmp[i]=*str;
+			i++;
+			str++;
+		}
+	}
+	return tmp;
+}
+
+/**
+ * 查找某个字符在字符串中的位置
+ * @return index
+ * index=0 没有找到
+ * index>0 找到,并且位置是index-1
+ */
+int getIndex(char *str,char aim){
+	int index;
+	int i=1;
+	while(*str!='\0'){
+		if(*str==aim){
+			index=i;
+			break;
+		}else{
+			i++;
+		}
+		str++;
+	}
+	return index;
+}
+
+
 char* join(char *s1, char *s2)  {
     char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
     //in real code you would check for errors in malloc here
@@ -42,15 +87,15 @@ NODE * ParseConf(char *filePath){
 	head=NULL;
 	one_line = (char*)malloc( MAX_LINE_SIZE * sizeof(char) );
 	while( fgets(one_line, MAX_LINE_SIZE, fin) != NULL ){
-		//处理切割k v
-//		char *key=(char *)malloc(sizeof(char));
-//		char *value=(char *)malloc(sizeof(char));
-		char *tmp=one_line;
-		while(*tmp!='\n'){
-			printf("%c\n",*tmp);
-			++tmp;
-		}
-//		printf("%s\n",key);
+		char *key=(char *)malloc(sizeof(char));
+		//获取字符串长度
+		int len=strlen(one_line);
+		//过滤#
+		char *tmp=filterSharp(one_line,len);
+//		printf("%s\n",tmp);
+		//找到=位置
+		int index=getIndex(one_line,'=');
+
 		if(head==NULL){
 			p1->key=one_line;
 			head=p1;
@@ -72,7 +117,7 @@ int main(){
 
 	Lines=ParseConf(filePath);
 	while(Lines!=NULL){
-//		printf("%s",Lines->key);
+		printf("%s",Lines->key);
 		Lines=Lines->next;
 	}
 
